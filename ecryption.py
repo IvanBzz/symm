90a3eac4536e2957bb62afd7816fdcc63b54e775
Ivan Bezuglov <146391288+IvanBzz@users.noreply.github.com>
2025-06-08 15:35:18 +0300

    1 update caesar

--- ecryption.py
+++ ecryption.py
@@ -1,4 +1,17 @@
 def encrypt(k, m):
-    return ''.join(map(chr, [x + k for x in map(ord, m)]))
+    return ''.join(map(chr, [(ord(c) + k) % 65536 for c in m]))
 
-print(encrypt(2, 'Hello!'))
+def decrypt(k, m):
+    return ''.join(map(chr, [(ord(c) - k) % 65536 for c in m]))
+
+
+key = int(input("Введите ключ: "))
+text = input("Введите текст: ")
+
+
+encrypted_text = encrypt(key, text)
+print("Зашифрованный текст:", encrypted_text)
+
+
+decrypted_text = decrypt(key, encrypted_text)
+print("Расшифрованный текст:", decrypted_text)
