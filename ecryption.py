d067e4a7434bcca7176b0623c77fc2523bd2ebca
Ivan Bezuglov <146391288+IvanBzz@users.noreply.github.com>
2025-06-08 15:44:35 +0300

    2.1) wrong iteration

--- ecryption.py
+++ ecryption.py
@@ -1,17 +1,39 @@
-def encrypt(k, m):
-    return ''.join(map(chr, [(ord(c) + k) % 65536 for c in m]))
+def break_cipher(text):
+    import string
 
-def decrypt(k, m):
-    return ''.join(map(chr, [(ord(c) - k) % 65536 for c in m]))
+    russian_chars = set(
+        'АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ' +
+        'абвгдеёжзийклмнопрстуфхцчшщъыьэюя' +
+        string.punctuation + string.whitespace + '0123456789'
+    )
+
+    max_score = 0
+    decrypted_text = ''
+    probable_key = 0
+
+    for k in range(65536):
+        attempted_text = ''.join(
+            [chr((ord(c) - k) % 65536) for c in text]
+        )
+
+        
+        score = sum(char in russian_chars for char in attempted_text)
+
+        if score > max_score:
+            max_score = score
+            decrypted_text = attempted_text
+            probable_key = k
+
+    
+
+        if score == len(text):
+            break
+
+    print(f"Найденный ключ: {probable_key}")
+    return decrypted_text
 
 
-key = int(input("Введите ключ: "))
-text = input("Введите текст: ")
 
-
-encrypted_text = encrypt(key, text)
-print("Зашифрованный текст:", encrypted_text)
-
-
-decrypted_text = decrypt(key, encrypted_text)
-print("Расшифрованный текст:", decrypted_text)
+cipher_text = input("Введите зашифрованный текст: ")
+original_text = break_cipher(cipher_text)
+print("Расшифрованный текст:", original_text)
